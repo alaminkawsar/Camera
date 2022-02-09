eb727fa43c00bc240e5ac32c5354aaad2fa61b52
shohel.rana <shohel.rana@mymedicalhub.com>
2022-02-09 12:45:56 +0600

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Khayrul on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
